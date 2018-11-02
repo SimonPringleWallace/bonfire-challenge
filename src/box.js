916075f25a5ff83d5f744f00384c03dd7a516954
SimonPringleWallace <s.pringlewallace@gmail.com>
2018-11-01 23:07:48 -0400

    Add Box Animations
    - Using a :hover add Animations to Boxes to control, float effect on
    hover, fade in of 'view case study' button and the addition of a red
    border on top of the cards on hover.

--- src/box.js
+++ src/box.js
@@ -7,8 +7,11 @@ class BoxContainer extends Component {
     return (
       <React.Fragment>
         <div className='box'>
+          <div className='box-content'>
           <h3>Box Number {this.props.number}</h3>
           <p className='card-description'>useless subtext</p>
+        <button className='case-study'> view the case study</button>
+         </div>
         </div>
       </React.Fragment>
     )
