0e4018cb3d5056591fd9cf34d1bd093c9c8c22f5
SimonPringleWallace <s.pringlewallace@gmail.com>
2018-11-02 22:52:47 -0400

    Improve Hero Spacing on Max-width 460px
    Increase spacing between 'Why Hello There' and the paragraph below.

--- src/header.js
+++ src/header.js
@@ -5,8 +5,8 @@ export const Header = () => (
   <div className="header">
     <div className='logo-container'><img alt='logo' src={require('./images/LOGODESIGN.png')}/></div>
     <ul className='nav-options'>
-      <li><a className='home-button' href='www.google.com'>Home</a> </li>
-      <li><a className='away-button' href='www.google.com'>Away</a></li>
+      <li><a className='home-button' href='#'>Home</a> </li>
+      <li><a className='away-button' href='#'>Away</a></li>
     </ul>
   </div>
 )
