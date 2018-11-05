6704baa0554a46f06262ba115112ff133650f410
SimonPringleWallace <s.pringlewallace@gmail.com>
2018-11-04 22:54:11 -0500

    Add # Back to Menu Items

--- src/header.js
+++ src/header.js
@@ -7,8 +7,8 @@ export const Header = () => (
     <div className='logo-container'><img alt='logo' src={require('./images/LOGODESIGN.png')}/></div>
     <ul className='nav-options'>
       {/* additional menu options may be added here as list items */}
-      <li><a className='home-button' href=''>Home</a> </li>
-      <li><a className='away-button' href=''>Away</a></li>
+      <li><a className='home-button' href='#'>Home</a> </li>
+      <li><a className='away-button' href='#'>Away</a></li>
     </ul>
   </div>
 )
