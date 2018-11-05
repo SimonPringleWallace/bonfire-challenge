7a8ce87aa9704533dac6a86fc26f410b29514ad1
SimonPringleWallace <s.pringlewallace@gmail.com>
2018-11-04 22:42:34 -0500

    Remove hrefs on Buttons - They Were Reseting the Homepage

--- src/Boxes-Box_Container/box.js
+++ src/Boxes-Box_Container/box.js
@@ -8,9 +8,7 @@ const Box = ({title, subtext}) => {
           <div className='box-content'>
           <h3>{title}</h3>
           <p className='card-description'>{subtext}</p>
-          <a href='#'>
             <button className='case-study'> view the case study</button>
-          </a>
          </div>
         </div>
       </React.Fragment>
